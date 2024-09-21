92cfe8f41929b26ed9c3d22683f796abbfcf9294
Samuelfdm <samuelfdm14@gmail.com>
2024-09-21 16:49:11 -0500

    Creación de la clase Tarea y adición de dependencias para pruebas

--- AppTareas/src/main/java/edu/eci/cvds/AppTareas/AppTareasApplication.java
+++ AppTareas/src/main/java/edu/eci/cvds/AppTareas/AppTareasApplication.java
@@ -2,8 +2,10 @@ package edu.eci.cvds.AppTareas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class AppTareasApplication {
 
 	public static void main(String[] args) {
---
+++ AppTareas/src/test/java/edu/eci/cvds/AppTareas/model/TareaTest.java
@@ -0,0 +1,5 @@
+package edu.eci.cvds.AppTareas.model;
+
+public class TareaTest {
+
+}
