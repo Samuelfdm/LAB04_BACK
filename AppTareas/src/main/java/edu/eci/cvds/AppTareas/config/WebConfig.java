9e3261a31785031763e46078ace869decf744fa3
Samuelfdm <samuelfdm14@gmail.com>
2024-09-26 10:37:54 -0500

    Cambios de configuración para que corra el front

--- AppTareas/src/main/java/edu/eci/cvds/AppTareas/config/WebConfig.java
+++ AppTareas/src/main/java/edu/eci/cvds/AppTareas/config/WebConfig.java
@@ -12,7 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addMapping("/**")
                 .allowedOrigins("http://127.0.0.1:8081", "http://127.0.0.1:8000", "http://127.0.0.1:5500")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowedHeaders("Content-Type", "Authorization")
+                .allowedHeaders("*")
                 .allowCredentials(true);
     }
 }
