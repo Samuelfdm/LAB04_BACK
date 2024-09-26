fd8963a842e178fc14f839bc3a530d39f6e0bdb4
Samuelfdm <samuelfdm14@gmail.com>
2024-09-26 10:26:05 -0500

    Cambios de configuración para que corra el front

--- AppTareas/src/main/java/edu/eci/cvds/AppTareas/config/WebConfig.java
+++ AppTareas/src/main/java/edu/eci/cvds/AppTareas/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://127.0.0.1:8081", "http://127.0.0.1:5500")
+                .allowedOrigins("http://127.0.0.1:8081", "http://127.0.0.1:8000", "http://127.0.0.1:5500")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("Content-Type", "Authorization")
                 .allowCredentials(true);
