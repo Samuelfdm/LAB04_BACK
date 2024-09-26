f9b09c32cefda24163c14c971e3324fecb390f4e
Samuelfdm <samuelfdm14@gmail.com>
2024-09-26 11:31:04 -0500

    Adición de pruebas para controlador actualizar

--- AppTareas/src/main/java/edu/eci/cvds/AppTareas/config/WebConfig.java
+++ AppTareas/src/main/java/edu/eci/cvds/AppTareas/config/WebConfig.java
@@ -10,8 +10,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://127.0.0.1:8000", "http://127.0.0.1:8081", "http://127.0.0.1:5500")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedOrigins("http://127.0.0.1:8000", "http://localhost:8000", "http://127.0.0.1:8081", "http://127.0.0.1:5500")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
     }
